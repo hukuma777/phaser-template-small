3b840288f47731a134b81721ad57198ff652e43c
hukuma777 <pg175b046@gmail.com>
2017-07-12 11:59:32 +0900

    星とプレイヤー実装

--- _site/js/states/Game.js
+++ _site/js/states/Game.js
@@ -1,13 +1,19 @@
 // インスタンス変数、関数の定義
 MyGame.Game = function (game) {
+    // dude
+    this.dude = null;
     // Star
-    this.star = null;
+    this.stars = null;
+    // 敵のグループ
+    this.tekis = null;
     // カーソルオブジェクト
     this.cursors = null;
     // スペースキーの取得
     this.spaceKey = null;
-    // 文字の移動速度
-    this.speed = 200;
+    // dudeの移動速度
+    this.speed = 2000;
+    // 残りの星の数
+    this.starCount = 0;
 };
 
 // タイトル処理
@@ -17,11 +23,59 @@ MyGame.Game.prototype = {
         // 物理エンジンを起動
         this.physics.startSystem(Phaser.Physics.ARCADE);
 
-        // 作成
-        this.star = this.add.sprite(this.world.width/2, this.world.height/2, "star");
-        this.star.anchor.setTo(0.5);
-        // Arcade物理エンジンを設定
-        this.physics.enable(this.star);
+        // 新規のグループを作成してそのインスタンスをstarsに代入
+        this.stars = this.add.group();
+        this.tekis = this.add.group();
+
+        // 星を数える
+        this.starCount = 0;
+
+        //グループ内のオブジェクトに物理挙動を有効にする
+        this.stars.enableBody = true;
+        this.tekis.enableBody = true;
+
+        //配列の最後に追加
+        for(let i=0; i<100; i++){
+            this.starCount++;
+            let star = this.stars.create(this.rnd.integerInRange(80,560),
+                                        this.rnd.integerInRange(80, 280),
+                                         'star');
+
+            star.body.collideWorldBounds = true;
+            star.body.bounce.x = 1;
+            star.body.bounce.y = 1;
+
+        //  星の初速乱数
+            star.body.velocity.x = this.rnd.realInRange(-200, 200);
+            star.body.velocity.y = this.rnd.realInRange(-200, 200);
+        }
+
+
+    this.dude = this.add.sprite(this.world.width/2,
+                                this.world.height/2,
+                                'dude');
+    this.dude.frame = 4;
+    this.dude.animations.add('left',[0,1,2,3], 10, true);
+    this.dude.animations.add('right',[5,6,7,8], 10, true);
+
+    // アンカー中心
+    this.dude.anchor.setTo(0.5);
+
+
+    // 物理エンジンをdudeが使う
+    // ARCADEb物理エンジン＝簡易なもの。速度が速い
+    this.physics.arcade.enable(this.dude);
+
+
+    //物理エンジンを有効にするとbodyというプロパティーが使えるようになる
+    //velocity=速度。秒速？ピクセルで指定
+    //dude.body.velocity.x = 200;
+    //dude.body.velocity.y = 200;
+    this.dude.body.collideWorldBounds = true;
+    this.dude.body.bounce.x = 1;
+    this.dude.body.bounce.y = 1;
+
+
 
         // カーソルキーの作成
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -32,8 +86,25 @@ MyGame.Game.prototype = {
         MyGame.gameParams.score = 0;
     },
 
+
+// 星とった時の処理
+    pickStar: function(dude, star) {
+        star.kill();
+        MyGame.gameParams.AddScore(100);
+
+        this.starCount--;
+        if (this.starCount <= 0) {
+            this.physics.arcade.isPaused = true;
+            this.state.start("Clear", false);
+        }
+    },
+
     // 更新処理
     update: function() {
+        //dudeと星が重なったら、その星を消す処理（PｃｋStar）を呼び出す
+        this.physics.arcade.overlap(
+                this.dude, this.stars, this.pickStar, null, this);
+
         // 移動
         let newvel = new Phaser.Point(0,0);
         // 上
@@ -57,18 +128,8 @@ MyGame.Game.prototype = {
             // 速度が設定されていたら、速度をthis.speedに設定する
             newvel.setMagnitude(this.speed);
         }
-        this.star.body.velocity = newvel;
+        this.dude.body.velocity = newvel;
 
-        // スペースキーをチェック
-        this.star.tint = 0xffffff;
-        if (this.spaceKey.justDown) {
-            // スペースが押された瞬間だったら赤くする
-            this.star.tint = 0xff0000;
-        }
-        else if (this.spaceKey.isDown) {
-            // スペースが押しっぱなしの間、青にする
-            this.star.tint = 0x0000ff;
-        }
 
         // Oキーでゲームオーバー
         if (this.input.keyboard.isDown(Phaser.KeyCode.O)) {
